9d985e84a6e1d47ab0893649c3a28796732ced33
System Administrator <bstetson@visione.com>
2016-12-20 10:33:07 -0500

    First push, woooooo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  gameReady: boolean = false;
+  name: string = '';
+  jeopardy: number = 1;
+  players: any = [];
+  vals: number[] = [200, 400, 600, 800, 1000];
+  updatePlayer(p): void {
+    p.score += parseInt(p.roundScore);
+    p.roundScore = '';
+  }
+
+  addPlayer(p) {
+    this.players.push({ name: p, score: 0, roundScore: '' });
+    this.name = '';
+    if (this.players.length == 5) {
+      //   this.gameReady = true;
+    }
+  }
+
+  switchJeopardy(to) {
+    this.jeopardy = to;
+    if (to == 2) {
+      this.vals = [400, 800, 1200, 1600, 2000];
+    } else {
+      this.vals = [200, 400, 600, 800, 1000];
+    }
+  }
+
+  addPoints(p, howmany) {
+    p.score += howmany;
+  }
+
+  subPoints(p, howmany) {
+    p.score -= howmany;
+  }
+
+  save() {
+    window.localStorage.setItem('game', JSON.stringify(this.players));
+  }
+
+  recover() {
+    let game = window.localStorage.getItem('game');
+    if (game != null) {
+      this.players = JSON.parse(game);
+    }
+  }
 }
